9344b9f07c348eac174e534866a314f778132956
Simone-Gramuglia <simone.gramuglia@marconibari.edu.it>
2025-02-11 14:58:20 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,50 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+class Program {
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String[] args) {
+        int n, i, coppie, j, cornici;
+        boolean trovato;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.println("Dimensione vettore");
+        n = input.nextInt();
+        int[] v = new int[n];
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        for (i = 0; i <= n - 1; i++) {
+            v[i] = random.nextInt(7) + 4;
+            System.out.println(v[i]);
+        }
+        coppie = 0;
+        trovato = false;
+        while (n >= 2 && trovato == false) {
+            trovato = false;
+            for (i = 0; i <= n - 2; i++) {
+                for (j = i + 1; j <= n - 1; j++) {
+                    if (v[i] == v[j]) {
+                        n = elimina(v, n, j);
+                        n = elimina(v, n, i);
+                        coppie = coppie + 1;
+                        trovato = true;
+                    }
+                }
+            }
+            trovato = !trovato;
+        }
+        cornici = (int) ((double) coppie / 2);
+        System.out.println("cornici = " + cornici);
+    }
+    
+    public static int elimina(int[] v, int n, int posizione) {
+        int i, n2;
+
+        for (i = posizione; i <= n - 2; i++) {
+            v[i] = v[i + 1];
+        }
+        n2 = n - 1;
+        
+        return n2;
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
